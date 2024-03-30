443dce9370ec76695ccc1a0aa12b7a1a2469d1e4
Alex Qian <lihaoq@lihaoq.localdomain>
2024-03-30 00:12:03 -0700

    test still

---
+++ build/hal/CMakeFiles/hal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hal.dir/src/joystick.c.o"
+  "CMakeFiles/hal.dir/src/motionSensor.c.o"
+  "CMakeFiles/hal.dir/src/utils.c.o"
+  "libhal.a"
+  "libhal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- hal/include/utils.h
+++ hal/include/utils.h
@@ -1,6 +1,6 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
-static void runCommand(char*);
+void __attribute__((optimize("O0"))) runCommand(char*);
 
 #endif
