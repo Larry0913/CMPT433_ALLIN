b82578d95df6367754f0be8184a7b776ff685cb8
Alex Qian <lihaoq@lihaoq.localdomain>
2024-03-30 00:45:35 -0700

    updated utils function

--- hal/src/utils.c
+++ hal/src/utils.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
+#include <time.h>
+#include <sys/ioctl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <fcntl.h> 
+#include <unistd.h>
 
 void runCommand(char* command)
 {
@@ -21,4 +28,61 @@ void runCommand(char* command)
         printf(" command: %s\n", command);
         printf(" exit code: %d\n", exitCode);
     }
+}
+
+int readFromFileToScreen(char *fileName) { // From Brian
+    FILE *pFile = fopen(fileName, "r"); 
+    if (pFile == NULL) {
+        printf("ERROR: Unable to open file (%s) for read\n", fileName);
+        exit(-1); 
+    }
+    // Read string (line)
+    const int MAX_LENGTH = 1024; 
+    char buff[MAX_LENGTH]; 
+    fgets(buff, MAX_LENGTH, pFile);
+    // Close
+    fclose(pFile);
+    int ret = atoi(buff);
+    return ret;
+}
+
+void sleepForMs(long long delayInMs) { // from Brian
+    const long long NS_PER_MS = 1000 * 1000; 
+    const long long NS_PER_SECOND = 1000000000;
+    long long delayNs = delayInMs * NS_PER_MS; 
+    int seconds = delayNs / NS_PER_SECOND; 
+    int nanoseconds = delayNs % NS_PER_SECOND;
+    struct timespec reqDelay = {seconds, nanoseconds};
+        nanosleep(&reqDelay, (struct timespec *) NULL);
+}
+
+
+void writeToFile(char* fileName, char* value) {
+	FILE* file = fopen(fileName, "w");
+	int charWritten = fprintf(file, "%s", value);
+	if (charWritten <= 0) printf("ERROR WRITING DATA");
+	fclose(file);
+}
+
+int initI2cBus(char* bus, int address) // from Brian
+{
+    int i2cFileDesc = open(bus, O_RDWR);
+    int result = ioctl (i2cFileDesc, I2C_SLAVE, address);
+    if (result < 0) {
+        perror("I2C: Unable to set I2C device to slave address.");
+        exit(1);
+    }
+    return i2cFileDesc;
+}
+
+void writeI2cReg(int i2cFileDesc, unsigned char regAddr, unsigned char value) // from Brian
+{
+    unsigned char buff[2];
+    buff[0] = regAddr;
+    buff[1] = value;
+    int res = write(i2cFileDesc, buff, 2); 
+    if (res != 2) {
+        perror("I2C: Unable to write i2c register.");
+        exit(1); 
+    }   
 }
